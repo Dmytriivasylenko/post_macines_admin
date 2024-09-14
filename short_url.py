2dbf40c58eac091df4bfe02b6e4e0a16becfb6ee
Dmytriivasylenko <160059389+Dmytriivasylenko@users.noreply.github.com>
2024-09-14 23:02:50 +0200

    post_macines_admin

--- short_url.py
+++ short_url.py
@@ -1,15 +1,25 @@
 import json
+import os
 import string
 import random
+import motor
+from motor import motor_asyncio
 from fastapi import FastAPI, Request, Form
 from typing import Annotated
 from fastapi.templating import Jinja2Templates
 import aiofiles
 from starlette.responses import RedirectResponse
 
+
+
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+
+client = motor.motor_asyncio.AsyncIOMotorClient(
+    f"mongodb://{os.getenv('MONGO_USERNAME', 'root')}:{os.getenv('MONGO_PASSWORD', 'example')}@{os.getenv('MONGO_HOST', 'localhost')}:{os.getenv('MONGO_PORT', '27017')}/")
+#client = motor.motor_asyncio.AsyncIOMotorClient(f'mongodb://root:example@127.0.0.1:27017/')
+
 
 
 @app.get("/")
@@ -17,40 +27,20 @@ async def index(request: Request):
     return templates.TemplateResponse(request=request, name="index.html")
 
 
+
 @app.post("/")
 async def get_url(url: Annotated[str, Form()]):
     short_url = ''.join(
         random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(6))
-
-    async with aiofiles.open('filename', mode='w') as f:
-        await f.write(json.dumps({}))
-
-
-    async with aiofiles.open('filename', mode='r') as f:
-        contents = await f.read()
-        if contents:
-            db_dict = json.loads(contents)
-        else:
-            db_dict = {}
-
-
-    db_dict[short_url] = url
-
-
-    async with aiofiles.open('filename', mode='w') as f:
-        await f.write(json.dumps(db_dict))
+    new_doc = {"short_url": short_url, "long_url": url}
+    await client["url_shortener"]["urls"].insert_one(new_doc)
 
     return {"result": short_url}
 
 
 @app.get("/{short_url}")
 async def say_hello(short_url: str):
-    async with aiofiles.open('filename', mode='r') as f:
-        contents = await f.read()
-
-    db_dict = json.loads(contents)
-
-    if short_url in db_dict:
-        url = db_dict[short_url]
-        return RedirectResponse(url)
+    url_document = await client["url_shortener"]["urls"].find_one({"short_url": short_url})
+    res_url = url_document["long_url"]
+    return RedirectResponse(res_url)
 
